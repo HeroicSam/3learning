31a83abd6bbce455211e81a632da8ff31405eb20
HeroicSam <cyuen10@gmail.com>
2022-10-04 20:08:19 -0400

    completed lesson 5 -transformations

--- src/lessons/05-transform-objects/src/script.js
+++ src/lessons/05-transform-objects/src/script.js
@@ -10,10 +10,39 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+const group = new THREE.Group()
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000})
+)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00})
+)
+
+cube2.position.x = -1.5
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff})
+)
+
+cube3.position.x = 1.5
+
+group.add(cube1, cube2, cube3)
+
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+// Axes Helper
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper)
 
 /**
  * Sizes
